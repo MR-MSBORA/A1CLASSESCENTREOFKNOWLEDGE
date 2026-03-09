2b406262c6f0d51354154df8cb15e22b63431d61
Manish Singh <150698709+MR-MSBORA@users.noreply.github.com>
2026-03-10 01:48:01 +0530

    fix-19

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,7 +1,36 @@
+// // import axios from "axios";
+
+// // const api = axios.create({
+// //   baseURL: import.meta.env.VITE_API_URL + "/api/v1",
+// //   withCredentials: true,
+// // });
+
+// // let authToken = null;
+
+// // export const setToken = (token) => {
+// //   authToken = token;
+// //   if (token) {
+// //     api.defaults.headers.common["Authorization"] = `Bearer ${token}`;
+// //   } else {
+// //     delete api.defaults.headers.common["Authorization"];
+// //   }
+// // };
+
+// // api.interceptors.request.use((config) => {
+// //   if (authToken) {
+// //     config.headers["Authorization"] = `Bearer ${authToken}`;
+// //   }
+// //   return config;
+// // });
+
+// // export default api;
+
+
+
 // import axios from "axios";
 
 // const api = axios.create({
-//   baseURL: import.meta.env.VITE_API_URL + "/api/v1",
+//   baseURL: import.meta.env.VITE_API_URL,
 //   withCredentials: true,
 // });
 
@@ -9,6 +38,7 @@
 
 // export const setToken = (token) => {
 //   authToken = token;
+
 //   if (token) {
 //     api.defaults.headers.common["Authorization"] = `Bearer ${token}`;
 //   } else {
@@ -30,27 +60,8 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: import.meta.env.VITE_API_URL,
+  baseURL: "https://a1classescentreofknowledge-api.onrender.com/api/v1",
   withCredentials: true,
-});
-
-let authToken = null;
-
-export const setToken = (token) => {
-  authToken = token;
-
-  if (token) {
-    api.defaults.headers.common["Authorization"] = `Bearer ${token}`;
-  } else {
-    delete api.defaults.headers.common["Authorization"];
-  }
-};
-
-api.interceptors.request.use((config) => {
-  if (authToken) {
-    config.headers["Authorization"] = `Bearer ${authToken}`;
-  }
-  return config;
 });
 
 export default api;
