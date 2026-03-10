feec5f5a89a41c6fc6b807b19fb1800854460f74
Manish Singh <150698709+MR-MSBORA@users.noreply.github.com>
2026-03-10 12:00:33 +0530

    fix-20

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,67 +1,31 @@
-// // import axios from "axios";
-
-// // const api = axios.create({
-// //   baseURL: import.meta.env.VITE_API_URL + "/api/v1",
-// //   withCredentials: true,
-// // });
-
-// // let authToken = null;
-
-// // export const setToken = (token) => {
-// //   authToken = token;
-// //   if (token) {
-// //     api.defaults.headers.common["Authorization"] = `Bearer ${token}`;
-// //   } else {
-// //     delete api.defaults.headers.common["Authorization"];
-// //   }
-// // };
-
-// // api.interceptors.request.use((config) => {
-// //   if (authToken) {
-// //     config.headers["Authorization"] = `Bearer ${authToken}`;
-// //   }
-// //   return config;
-// // });
-
-// // export default api;
-
-
-
-// import axios from "axios";
-
-// const api = axios.create({
-//   baseURL: import.meta.env.VITE_API_URL,
-//   withCredentials: true,
-// });
-
-// let authToken = null;
-
-// export const setToken = (token) => {
-//   authToken = token;
-
-//   if (token) {
-//     api.defaults.headers.common["Authorization"] = `Bearer ${token}`;
-//   } else {
-//     delete api.defaults.headers.common["Authorization"];
-//   }
-// };
-
-// api.interceptors.request.use((config) => {
-//   if (authToken) {
-//     config.headers["Authorization"] = `Bearer ${authToken}`;
-//   }
-//   return config;
-// });
-
-// export default api;
-
-
-
 import axios from "axios";
 
+const BASE = import.meta.env.VITE_API_URL
+  ? import.meta.env.VITE_API_URL.replace(/\/$/, "") + "/api/v1"
+  : "https://a1classescentreofknowledge-api.onrender.com/api/v1";
+
 const api = axios.create({
-  baseURL: "https://a1classescentreofknowledge-api.onrender.com/api/v1",
+  baseURL: BASE,
   withCredentials: true,
+});
+
+let authToken = null;
+
+export const setToken = (token) => {
+  authToken = token;
+  if (token) {
+    api.defaults.headers.common["Authorization"] = `Bearer ${token}`;
+  } else {
+    delete api.defaults.headers.common["Authorization"];
+  }
+};
+
+// Attach token on every request (safety net)
+api.interceptors.request.use((config) => {
+  if (authToken) {
+    config.headers["Authorization"] = `Bearer ${authToken}`;
+  }
+  return config;
 });
 
 export default api;
